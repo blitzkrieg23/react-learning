b6a101e623d550c97fce1f25a31e65921a8412be
Prashant Bhatt <prashant.bhatt.ext@siemens.com>
2021-05-15 01:53:47 +0530

    conditions list and charts

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,19 @@
 import Card from '../UI/Card';
-import ExpenseItem from './ExpenseItem';
+// import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
 import React, { useState } from 'react';
-
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState('2020');
 
+  const filteredExpenses = props.items.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    console.log('Selected Date is', selectedYear);
   };
+
   return (
     <div>
       <Card className="expenses">
@@ -17,7 +21,37 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
+        <ExpensesChart expenses={filteredExpenses}></ExpensesChart>
+        <ExpensesList items={filteredExpenses} />
+        {/* {expensesContent} */}
+        {/* approcah 1 */}
+        {/* {filteredExpenses.length === 0 ? (
+          <p>No content bro</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )} */}
+
+        {/* approcah 2 */}
+        {/* {filteredExpenses.length === 0 && <p>No content bro</p>}
+        {filteredExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))} */}
+
+        {/* approcah 3 */}
+        {/* <ExpenseItem
           title={props.item[0].title}
           amount={props.item[0].amount}
           date={props.item[0].date}
@@ -36,7 +70,7 @@ function Expenses(props) {
           title={props.item[3].title}
           amount={props.item[3].amount}
           date={props.item[3].date}
-        ></ExpenseItem>
+        ></ExpenseItem> */}
       </Card>
     </div>
   );
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -44,7 +44,7 @@ const ExpenseForm = (props) => {
     event.preventDefault();
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
     // console.log(expenseData);
@@ -86,6 +86,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
+        <button onClick={props.onStopEditing} type="button">
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
