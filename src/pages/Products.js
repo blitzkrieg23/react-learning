9fc77b97c11c6dbfce8fdfa7c09590c910b20923
Prashant Bhatt <prashant.bhatt.ext@siemens.com>
2021-05-31 14:44:02 +0530

    deleting files

--- src/pages/Products.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const Products = () => {
-  return <h1>Hello this is Products page</h1>;
-};
-export default Products;
--- src/pages/Welcome.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const Welcome = () => {
-  return <h1>Hello this is welcome page</h1>;
-};
-export default Welcome;
