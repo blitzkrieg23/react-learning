506d5fcb3b94aeb4350434f7c40d4d06fd3d5e32
Prashant Bhatt <prashant.bhatt.ext@siemens.com>
2021-06-01 23:41:32 +0530

    Dynamic routes nested routes done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Redirect, Route, Switch } from 'react-router';
+import AllQuotes from './pages/AllQuotes';
+import NewQuote from './pages/NewQuote';
+import QuoteDetail from './pages/QuoteDetail';
+
 function App() {
   return (
     <div>
-
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes"></Redirect>
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuotes></AllQuotes>
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail></QuoteDetail>
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote></NewQuote>
+        </Route>
+      </Switch>
     </div>
   );
 }
